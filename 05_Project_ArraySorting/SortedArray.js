031e6f523ac46bb44db3904539e442389e88e4cf
AVISHERE <akashvyas2006@gmail.com>
2024-10-26 16:18:06 +0530

    Array Sortner With JS

---
+++ 05_Project_ArraySorting/SortedArray.js
@@ -0,0 +1,24 @@
+hello = document.getElementById('hello');
+InputElements = document.getElementById('InputElements');
+let array = [20,10,11,80,3,2,89,1,0,];
+
+function sorter(numbers){
+
+    for(j = 0 ; j < numbers.length  ; j++ ){
+        for(i = 0 ; i < numbers.length + 1  ; i++ ){
+        if(numbers[i]>numbers[i + 1]){
+        let temp = numbers[i];
+        numbers[i] = numbers[i + 1];
+         numbers[i + 1] = temp;
+    }
+}
+}
+
+    return numbers;
+}
+function sort(){
+    array = InputElements.value.split(" ").map(Number);
+    hello.innerHTML = `Sorted Array: ${sorter(array).join(", ")}`;
+}
+
+// hello.innerHTML = `${sorter(array)}`;

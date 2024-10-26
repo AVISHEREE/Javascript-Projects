5da96552e7ca8574a356c85c1b114efb8b08e1fd
AVISHERE <akashvyas2006@gmail.com>
2024-10-26 15:10:01 +0530

    Notes  With JS

---
+++ 02_Project_NotesTaking/Notes.js
@@ -0,0 +1,48 @@
+let inputNoteTittle = document.querySelector("#inputNoteTittle");
+let myForm = document.querySelector("#myForm");
+let msg = document.querySelector("#msg");                                   
+let noteContainer = document.querySelector(".noteContainer");
+// let text = document.querySelector(".text");
+
+myForm.addEventListener ("submit" , (a) =>{
+    a.preventDefault();
+    if(inputNoteTittle.value === ''){
+        msg.innerHTML = "please enter a tittle here";
+    }else{
+   addNote();
+    }
+})
+let addNote = () =>{
+    tittle = inputNoteTittle.value;
+    noteContainer.innerHTML += `
+    <div>
+    <h4 id="">${tittle}</h4>
+    <textarea name=""  cols="20" rows="7" class="text"></textarea>
+    <button name="" onClick="deleteList(this)" id="btn" type="button" >&times</button>
+    <button name="" onClick="editList(this)" id="btn" type="button" >Edit the tittle </button>
+    </div>
+    `
+    inputNoteTittle.value = '';
+}
+let deleteList = (d) => {
+    d.parentElement.remove();
+}
+let editList = (e) => {
+    if(inputNoteTittle.value === ''){
+        inputNoteTittle.value = e.parentElement.querySelector('.text').value.trim()
+        msg.innerHTML = "please enter the tittle here and then click on edit the tittle btn"
+    }else{
+        text = e.parentElement.querySelector('.text').value.trim();
+        tittle = inputNoteTittle.value;
+    e.parentElement.innerHTML = `
+    <div>
+    <h4>${tittle}</h4>
+    <textarea name=""   class="text" cols="20" rows="7">${text}</textarea>
+    <button name="" onClick="deleteList(this)" id="btn" type="button" >&times</button>
+    <button name="" onClick="editList(this)" id="btn" type="button" >Edit the tittle </button>
+    </div>
+    `
+    inputNoteTittle.value = ''; 
+    msg.innerHTML = ''; 
+    }
+}

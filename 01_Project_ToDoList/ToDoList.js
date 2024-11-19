642e19dd0908af1ca0e99df522e5e2ea3a6f734f
AVISHERE <akashvyas2006@gmail.com>
2024-11-19 18:30:23 +0530

    CSS and JS files Updated

--- 01_Project_ToDoList/ToDoList.js
+++ 01_Project_ToDoList/ToDoList.js
@@ -14,17 +14,25 @@ let deleteList = (e) => {
     e.parentElement.remove();
 };
 let editList = (a) => {
-    if(addListTitle.value === ''){
-        msg.innerHTML = 'please enter the data here then click on the edit icon';
+    if(addListTitle.value.trim() === ''){
+        msg.style.display = "block";
+        msg.innerHTML = 'please enter the data in input then click on the edit icon';
+        let text = a.parentElement.children[1];
+        addListTitle.value = text.innerText;
     }else{
+        if (a.parentElement.id === 'listHolder') {
+            a.parentElement.id = '';
+        }
         a.parentElement.innerHTML = `<div id="listHolder">
         <input type="checkbox" name="" id="">
          <li type="none">   ${addListTitle.value} </li>
          <button name="" onClick="deleteList(this)" id="btn" type="button" >&times</button>
          <button name="" onClick="editList(this)" id="btn" type="button" >&#x270E;</button>
          `;
-         msg.innerHTML = '';
-    addListTitle.value = '';
+        msg.innerHTML = '';
+        msg.style.display = "none"
+        addListTitle.value = '';
+        
     }
 };
 
